bce252fe06e690664a5a2021aff5b6e2d88ed609
yongxiangng <yongxiangng@gmail.com>
2021-05-26 15:38:36 +0800

    Login page

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -11,3 +11,29 @@ const textInputDecoration = InputDecoration(
     borderSide: BorderSide(color: Colors.pink, width: 2.0),
   ),
 );
+
+// constants for colors
+
+extension HexColor on Color {
+  /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
+  static Color fromHex(String hexString) {
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
+
+  /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
+  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
+      '${alpha.toRadixString(16).padLeft(2, '0')}'
+      '${red.toRadixString(16).padLeft(2, '0')}'
+      '${green.toRadixString(16).padLeft(2, '0')}'
+      '${blue.toRadixString(16).padLeft(2, '0')}';
+}
+
+// ignore: non_constant_identifier_names
+Color BACKGROUND_COLOR = HexColor.fromHex('FDFDFD');
+// ignore: non_constant_identifier_names
+Color GREEN_1 = HexColor.fromHex('BFE1D7');
+// ignore: non_constant_identifier_names
+Color ORANGE_1 = HexColor.fromHex('E1B58F');
