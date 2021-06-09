5f3490f1182f49e7b25a684cdf5f957ea86fa07a
yongxiangng <yongxiangng@gmail.com>
2021-06-10 00:26:59 +0800

    Initialise maps location to Singapore

--- lib/screens/home/maps.dart
+++ lib/screens/home/maps.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -8,15 +10,47 @@ class Maps extends StatefulWidget {
 
 class _MapsState extends State<Maps> {
   @override
-  Widget build(BuildContext context) {
-    CameraPosition _cameraPosition = CameraPosition(
-      target: LatLng(22.5448131, 88.3403691),
-      zoom: 15,
-    );
+  void initState() {
+    super.initState();
+    _getUserLocation();
+  }
 
+  late Completer<GoogleMapController> controller1;
+
+  final Set<Marker> _markers = {};
+  MapType _currentMapType = MapType.normal;
+  static LatLng _currentPosition = LatLng(1.290270, 103.851959);
+
+  void _getUserLocation() async {
+    var position = await GeolocatorPlatform.instance
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(_currentPosition);
+    setState(() {
+      _currentPosition = LatLng(position.latitude, position.longitude);
+    });
+  }
+
+  _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      controller1.complete(controller);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       child: GoogleMap(
-        initialCameraPosition: _cameraPosition,
+        markers: _markers,
+        mapType: _currentMapType,
+        initialCameraPosition: CameraPosition(
+          target: _currentPosition,
+          zoom: 14.4746,
+        ),
+        onMapCreated: _onMapCreated,
+        zoomGesturesEnabled: true,
+        myLocationEnabled: true,
+        compassEnabled: true,
+        myLocationButtonEnabled: false,
       ),
     );
   }
