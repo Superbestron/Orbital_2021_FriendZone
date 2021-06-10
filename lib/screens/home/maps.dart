331d7df95e25f798bb37a1f53a48a599dc1a689c
yongxiangng <yongxiangng@gmail.com>
2021-06-10 09:48:56 +0800

    Request for location perms

--- lib/screens/home/maps.dart
+++ lib/screens/home/maps.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:myapp/shared/loading_transparent.dart';
 
 class Maps extends StatefulWidget {
   @override
@@ -15,22 +16,62 @@ class _MapsState extends State<Maps> {
     _getUserLocation();
   }
 
-  late Completer<GoogleMapController> controller1;
+  Completer<GoogleMapController> controller1 = Completer();
 
   final Set<Marker> _markers = {};
   MapType _currentMapType = MapType.normal;
-  static LatLng _currentPosition = LatLng(1.290270, 103.851959);
+  static LatLng _initialPosition = LatLng(0, 0);
+  bool initialised = false;
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
 
   void _getUserLocation() async {
-    var position = await GeolocatorPlatform.instance
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print(_currentPosition);
+    var position = await _determinePosition();
+    initialised = true;
+    print(_initialPosition);
     setState(() {
-      _currentPosition = LatLng(position.latitude, position.longitude);
+      _initialPosition = LatLng(position.latitude, position.longitude);
     });
   }
 
   _onMapCreated(GoogleMapController controller) {
+    _getUserLocation();
     setState(() {
       controller1.complete(controller);
     });
@@ -38,20 +79,21 @@ class _MapsState extends State<Maps> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: GoogleMap(
-        markers: _markers,
-        mapType: _currentMapType,
-        initialCameraPosition: CameraPosition(
-          target: _currentPosition,
-          zoom: 14.4746,
-        ),
-        onMapCreated: _onMapCreated,
-        zoomGesturesEnabled: true,
-        myLocationEnabled: true,
-        compassEnabled: true,
-        myLocationButtonEnabled: false,
-      ),
-    );
+    return initialised
+        ? Container(
+            child: GoogleMap(
+              markers: _markers,
+              mapType: _currentMapType,
+              initialCameraPosition: CameraPosition(
+                target: _initialPosition,
+                zoom: 14.4746,
+              ),
+              onMapCreated: _onMapCreated,
+              zoomGesturesEnabled: true,
+              myLocationEnabled: true,
+              compassEnabled: true,
+            ),
+          )
+        : TransparentLoading();
   }
 }
