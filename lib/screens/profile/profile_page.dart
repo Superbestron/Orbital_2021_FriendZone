4968b98bc3438b1779df29e38aa7f4cf6849a71f
yongxiangng <yongxiangng@gmail.com>
2021-06-16 20:40:25 +0800

    Send friend request feature

--- lib/screens/profile/profile_page.dart
+++ lib/screens/profile/profile_page.dart
@@ -18,7 +18,6 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-
   ImageProvider? _profileImage;
 
   @override
@@ -34,11 +33,11 @@ class _ProfilePageState extends State<ProfilePage> {
         print('no image');
         _profileImage = DEFAULT_PROFILE_PIC;
       } else {
-        dbService.getImageURLFromFirebase(profileImagePath).then((url) =>
-            setState(() {
-              _profileImage = NetworkImage(url);
-            })
-        );
+        dbService
+            .getImageURLFromFirebase(profileImagePath)
+            .then((url) => setState(() {
+                  _profileImage = NetworkImage(url);
+                }));
       }
     }
 
@@ -60,14 +59,12 @@ class _ProfilePageState extends State<ProfilePage> {
                   image: _profileImage ?? DEFAULT_PROFILE_PIC,
                   onClicked: () async {
                     if (isSelf) {
-                      final _image = await Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) {
-                            return EditProfilePage(
-                                userData: userData,
-                                profileImage: _profileImage ?? DEFAULT_PROFILE_PIC
-                            );
-                          })
-                      );
+                      final _image = await Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return EditProfilePage(
+                            userData: userData,
+                            profileImage: _profileImage ?? DEFAULT_PROFILE_PIC);
+                      }));
                       setState(() {
                         _profileImage = _image;
                       });
@@ -76,16 +73,29 @@ class _ProfilePageState extends State<ProfilePage> {
                 ),
                 const SizedBox(height: 24),
                 buildName(userData),
-                const SizedBox(height: 36),
+                isSelf
+                    ? SizedBox(height: 36)
+                    : Column(
+                        children: [
+                          ElevatedButton(
+                              child: Text('Add friend',
+                                  style: TextStyle(color: Colors.white)),
+                              style: ElevatedButton.styleFrom(
+                                primary: ORANGE_1,
+                              ),
+                              onPressed: () {
+                                // user send friend request to profile
+                                dbService.addRelation(userID, widget.profileID);
+                              })
+                        ],
+                      ),
                 NumbersWidget(points: userData.points, level: userData.level),
                 const SizedBox(height: 36),
                 buildAbout(userData),
                 const SizedBox(height: 36),
-                SvgPicture.asset(
-                    'assets/tree.svg',
+                SvgPicture.asset('assets/tree.svg',
                     // fit: BoxFit.cover,
-                    clipBehavior: Clip.hardEdge
-                ),
+                    clipBehavior: Clip.hardEdge),
               ],
             ),
           );
@@ -98,110 +108,109 @@ class _ProfilePageState extends State<ProfilePage> {
 }
 
 Widget buildName(UserData userData) => Column(
-  children: [
-    Text(userData.name,
-      style: TEXT_FIELD_HEADING,
-    ),
-    const SizedBox(height: 4),
-    Text(userData.faculty,
-      style: TextStyle(color: Colors.grey),
-    )
-  ],
-);
+      children: [
+        Text(
+          userData.name,
+          style: TEXT_FIELD_HEADING,
+        ),
+        const SizedBox(height: 4),
+        Text(
+          userData.faculty,
+          style: TextStyle(color: Colors.grey),
+        )
+      ],
+    );
 
 class NumbersWidget extends StatelessWidget {
-
   final int points;
   final int level;
 
-  const NumbersWidget({
-    Key? key,
-    required this.points,
-    required this.level
-  }) : super(key: key);
+  const NumbersWidget({Key? key, required this.points, required this.level})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) => Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      Column(
-        children: [
-          Icon(Icons.star),
-          buildButton(context, '$points', 'Points'),
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            children: [
+              Icon(Icons.star),
+              buildButton(context, '$points', 'Points'),
+            ],
+          ),
+          buildDivider(),
+          Column(
+            children: [
+              Icon(Icons.trending_up),
+              buildButton(context, '$level', 'Level'),
+            ],
+          ),
+          buildDivider(),
+          // TODO: Possible button
+          // buildButton(context, '$friends', 'Friends'),
+          Column(
+            children: [
+              Icon(Icons.social_distance),
+              buildButton(context, '50', 'Friends'),
+            ],
+          ),
         ],
-      ),
-      buildDivider(),
-      Column(
-        children: [
-          Icon(Icons.trending_up),
-          buildButton(context, '$level', 'Level'),
-        ],
-      ),
-      buildDivider(),
-      // TODO: Possible button
-      // buildButton(context, '$friends', 'Friends'),
-      Column(
-        children: [
-          Icon(Icons.social_distance),
-          buildButton(context, '50', 'Friends'),
-        ],
-      ),
-    ],
-  );
+      );
 
   Widget buildDivider() => Container(
-    height: 24,
-    child: VerticalDivider(color: Colors.grey),
-  );
+        height: 24,
+        child: VerticalDivider(color: Colors.grey),
+      );
 
   Widget buildButton(BuildContext context, String value, String text) =>
-    MaterialButton(
-      padding: EdgeInsets.symmetric(vertical: 4),
-      onPressed: () {},
-      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Text(value,
-            style: TEXT_FIELD_HEADING,
-          ),
-          SizedBox(height: 2),
-          Text(
-            text,
-            style: NORMAL,
-          ),
-        ],
-      ),
-    );
+      MaterialButton(
+        padding: EdgeInsets.symmetric(vertical: 4),
+        onPressed: () {},
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              value,
+              style: TEXT_FIELD_HEADING,
+            ),
+            SizedBox(height: 2),
+            Text(
+              text,
+              style: NORMAL,
+            ),
+          ],
+        ),
+      );
 }
 
 Widget buildAbout(UserData userData) => Container(
-  padding: EdgeInsets.symmetric(horizontal: 48),
-  child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Row(
+      padding: EdgeInsets.symmetric(horizontal: 48),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('About', style: TEXT_FIELD_HEADING),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Icon(Icons.short_text),
+          Row(
+            children: [
+              Text('About', style: TEXT_FIELD_HEADING),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Icon(Icons.short_text),
+              )
+            ],
+          ),
+          const SizedBox(height: 16),
+          Container(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: 100,
+                minWidth: 500,
+              ),
+              child: Text(userData.bio),
+            ),
+            decoration: boxDecoration,
+            padding: const EdgeInsets.all(15.0),
           )
         ],
       ),
-      const SizedBox(height: 16),
-      Container(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-            minHeight: 100,
-            minWidth: 500,
-          ),
-          child: Text(userData.bio),
-        ),
-        decoration: boxDecoration,
-        padding: const EdgeInsets.all(15.0),
-      )
-    ],
-  ),
-);
+    );
