92da8a76b13271d361c78e2b362f9af530b0ca33
Ng Yong Xiang <60974969+yongxiangng@users.noreply.github.com>
2021-07-12 18:59:57 +0800

    Users now unable to try attend an event when it has passed (#30)
    * Add error message when user tries to attend past events
    
    * remove typo

--- lib/screens/event/attendee_tile.dart
+++ lib/screens/event/attendee_tile.dart
@@ -4,6 +4,7 @@ import 'package:myapp/screens/profile/profile_page.dart';
 import 'package:myapp/services/database.dart';
 import 'package:myapp/shared/constants.dart';
 import 'package:myapp/shared/loading_transparent.dart';
+import 'package:myapp/models/event.dart';
 import 'package:myapp/shared/widgets.dart';
 import 'package:provider/provider.dart';
 
@@ -75,7 +76,11 @@ class _AttendeeTileState extends State<AttendeeTile> {
                   Scaffold(
                     // AppBar that is shown on event_page
                     appBar: buildAppBar('User\'s Profile'),
-                    body: ProfilePage(profileID: widget.attendee.uid == uid ? "" : widget.attendee.uid))
+                    body: StreamProvider<List<Event>?>.value(
+                        initialData: null,
+                        value: DatabaseService.events,
+                        child:ProfilePage(profileID: widget.attendee.uid == uid ? "" : widget.attendee.uid))
+                  )
               ]),
             ),
           ),
