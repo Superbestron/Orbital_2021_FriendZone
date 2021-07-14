0d1a6c9b472655df165669ad202c719fc7137f98
yongxiangng <yongxiangng@gmail.com>
2021-07-14 21:14:35 +0800

    Fix failing test cases

--- test/screens/authenticate/sign_in_test.dart
+++ test/screens/authenticate/sign_in_test.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:myapp/main.dart';
+import 'package:myapp/screens/authenticate/sign_in.dart';
 import '../../mock.dart'; // from: https://github.com/FirebaseExtended/flutterfire/blob/master/packages/firebase_auth/firebase_auth/test/mock.dart
 
 void main() async {
@@ -13,41 +14,35 @@ void main() async {
   });
 
   // widget tests
-  // testWidgets('sign in button render', (WidgetTester tester) async {
-  //   await tester.pumpWidget(
-  //       MyApp()
-  //   );
-  //   final button = find.widgetWithText(ElevatedButton, "Sign In");
-  //   expect(button, findsOneWidget);
-  // });
-  //
-  // testWidgets('register button render', (WidgetTester tester) async {
-  //   await tester.pumpWidget(
-  //       MyApp()
-  //   );
-  //   final button = find.widgetWithText(GestureDetector, "Register");
-  //   expect(button, findsOneWidget);
-  // });
-  //
-  // testWidgets('no error message', (WidgetTester tester) async {
-  //   await tester.pumpWidget(
-  //     MyApp()
-  //   );
-  //   final msg = find.widgetWithText(Text, "Incorrect Email/Password!");
-  //   expect(msg, findsNothing);
-  // });
-  //
-  // testWidgets('empty sign in', (WidgetTester tester) async {
-  //   await tester.pumpWidget(
-  //       MyApp()
-  //   );
-  //   final button = find.widgetWithText(ElevatedButton, "Sign In");
-  //   await tester.tap(button);
-  //   await tester.pump();
-  //   final error1 = find.text("Enter a NUS email");
-  //   final error2 = find.text("Enter a password 6+ chars long");
-  //   expect(error1, findsOneWidget);
-  //   expect(error2, findsOneWidget);
-  // });
+  testWidgets('sign in button render', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: SignIn(toggleView: () {})));
+    final button = find.text("Sign In");
+    expect(button, findsOneWidget);
+  });
 
+  testWidgets('register button render', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: SignIn(toggleView: () {})));
+    final button = find.widgetWithText(GestureDetector, "Register");
+    expect(button, findsOneWidget);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+  });
+
+  testWidgets('no error message', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: SignIn(toggleView: () {})));
+    final msg = find.widgetWithText(Text, "Incorrect Email/Password!");
+    expect(msg, findsNothing);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+  });
+
+  testWidgets('empty sign in', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: SignIn(toggleView: () {})));
+    final button = find.widgetWithText(ElevatedButton, "Sign In");
+    await tester.tap(button);
+    await tester.pump();
+    final error1 = find.text("Enter a NUS email");
+    final error2 = find.text("Enter a password 6+ chars long");
+    expect(error1, findsOneWidget);
+    expect(error2, findsOneWidget);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+  });
 }
